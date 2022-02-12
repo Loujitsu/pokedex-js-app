ae62e84ea1d4c1b999e1f1bc8ba471ca07d32cac
Loujitsu <53363924+Loujitsu@users.noreply.github.com>
2022-02-11 20:30:57 -0700

    Replaced the (semi colon) ; with a (comma) ,

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,31 @@
 pokemonList = [{
-  name: 'Articuno';
-  height: 1.7;
-  weight: 55.4;
+  name: 'Articuno',
+  height: 1.7,
+  weight: 55.4,
   type: ['fire', 'flying']
 },
 {
-  name: 'Zaptos';
-  height: 1.6
-  weight: 52.6;
+  name: 'Zaptos',
+  height: 1.6,
+  weight: 52.6,
   type: ['electric', 'flying']
 },
 {
-  name: 'Moltres'
-  height: 2;
-  weight: 60;
+  name: 'Moltres',
+  height: 2,
+  weight: 60,
   type: ['fire', 'flying']
 },
 {
-  name: : 'Lugia';
-  height: 5.2;
-  weight: 216;
+  name: : 'Lugia',
+  height: 5.2,
+  weight: 216,
   type: ['pyschic', 'flying']
 },
 {
-  name: 'Ho-oh'
-  height: 3.8;
-  weight: 199;
+  name: 'Ho-oh',
+  height: 3.8,
+  weight: 199,
   type: ['fire', 'flying']
 }
 ]
